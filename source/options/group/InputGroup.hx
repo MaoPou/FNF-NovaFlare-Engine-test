9253ad6bb4043c99fc7f2143fec667cba0ccd3b7
MaoPou <166735337+MaoPou@users.noreply.github.com>
2024-09-17 11:20:15 +0800

    Update InputGroup.hx

--- source/options/group/InputGroup.hx
+++ source/options/group/InputGroup.hx
@@ -22,6 +22,13 @@ class InputGroup
         follow.addOption(option);
 
         var option:Option = new Option(
+            'password',
+            'PasswordState',
+            STATE
+        );
+        follow.addOption(option);
+
+        var option:Option = new Option(
             Language.getStr('TouchMain'),
             TEXT
         );
