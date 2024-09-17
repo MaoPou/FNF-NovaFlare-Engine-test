90a6a717607d6f3d69d3d93481b824db44e91b90
MaoPou <166735337+MaoPou@users.noreply.github.com>
2024-09-17 11:16:39 +0800

    Add files via upload

--- source/states/PasswordState.hx
+++ source/states/PasswordState.hx
@@ -1,33 +1,61 @@
 package states;
 
-import flixel.addons.ui.FlxUIInputText;
+import flixel.FlxSubState;
+
 import flixel.addons.transition.FlxTransitionableState;
 import states.MainMenuState;
 
+import flixel.FlxState;
+import flixel.FlxG;
+import flixel.ui.FlxTextInput;
+import flixel.ui.FlxText;
+
 class PasswordState extends MusicBeatState
 {
-    private var password:FlxUIInputText;
-	                                                                                                                                                                                              private var fuckpass = "immaopou";
-    override function create()
-	{
-		super.create();
-		password = new FlxUIInputText(200, 200, 500, "Please Enter PassWord");
-		add(password);
-		
-		addVirtualPad(NONE, A);
-	}
-	
-	override function update(elapsed:Float)
-	{
-	    if (controls.ACCEPT)
-	    {
-	        if (password.text == fuckpass)
-	        {
-	            MusicBeatState.switchState(new TitleState());
-	            ClientPrefs.data.needpass = false;
-	        }
-	    }
-	
-	    super.update(elapsed);
-	}
+    private var passwordInput:FlxTextInput;
+    private var feedbackText:FlxText;
+    private var correctPassword:String = "maopouyyds";
+
+    override public function create():Void
+    {
+        super.create();
+
+        var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
+		add(bg);
+
+        // 创建密码输入框
+        passwordInput = new FlxTextInput(50, 50, 200);
+        add(passwordInput);
+
+        // 创建反馈文本
+        feedbackText = new FlxText(50, 100, 200, "");
+        feedbackText.setFormat(null, 16, FlxG.WHITE, "center");
+        add(feedbackText);
+        
+        addVirtualPad(NONE, A);
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+
+        if if (controls.ACCEPT)
+        {
+            if (passwordInput.text == correctPassword)
+            {
+                feedbackText.text = "Password Correct!";
+
+                onPasswordCorrect();
+            }
+            else
+            {
+                feedbackText.text = "Incorrect Password!";
+            }
+        }
+    }
+
+    private function onPasswordCorrect():Void
+    {
+        MusicBeatState.switchState(new MainMenuState());
+    }
 }
