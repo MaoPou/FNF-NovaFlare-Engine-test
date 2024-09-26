b774ce9b8f7592b079b685bed9284639b957b63b
MaoPou <166735337+MaoPou@users.noreply.github.com>
2024-09-26 22:13:03 +0800

    Update UIGroup.hxbshsjsjjs

--- source/options/group/UIGroup.hx
+++ source/options/group/UIGroup.hx
@@ -42,13 +42,6 @@ class UIGroup
         follow.addOption(option);
 
         var option:Option = new Option(
-            Language.getStr('opponentStrums'),
-            'opponentStrums',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
             Language.getStr('judgementCounter'),
             'judgementCounter',
             BOOL
@@ -76,100 +69,6 @@ class UIGroup
             'timeBarType',
             STRING,
             TimeBarArray
-        );
-        follow.addOption(option);
-
-        ///////////////////////////////
-
-        var option:Option = new Option(
-            Language.getStr('HealthBar'),
-            TEXT
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('healthBarAlpha'),
-            'healthBarAlpha',
-            FLOAT,
-            0,
-            1,
-            1
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('oldHealthBarVersion'),
-            'oldHealthBarVersion',
-            BOOL
-        );
-        follow.addOption(option);
-
-        ///////////////////////////////
-
-        var option:Option = new Option(
-            Language.getStr('Combo'),
-            TEXT
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('comboColor'),
-            'comboColor',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('comboOffsetFix'),
-            'comboOffsetFix',
-            BOOL
-        );
-        follow.addOption(option);
-
-        ///////////////////////////////
-
-        var option:Option = new Option(
-            Language.getStr('KeyBoard'),
-            TEXT
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('keyboardAlpha'),
-            'keyboardAlpha',
-            FLOAT,
-            0,
-            1,
-            1
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('keyboardTime'),
-            'keyboardTime',
-            INT,
-            0,
-            1000,
-            0,
-            'MS'
-        );
-        follow.addOption(option);
-
-        var colorStingArray = ['BLACK', 'WHITE', 'GRAY', 'RED', 'GREEN', 'BLUE', 'YELLOW', 'PINK', 'ORANGE', 'PURPLE', 'BROWN', 'CYAN'];
-
-        var option:Option = new Option(
-            Language.getStr('keyboardBGColor'),
-            'keyboardBGColor',
-            STRING,
-            colorStingArray
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('keyboardTextColor'),
-            'keyboardTextColor',
-            STRING,
-            colorStingArray
         );
         follow.addOption(option);
 
