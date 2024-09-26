7a12124d27d15312600c6f72068595770336e861
MaoPou <166735337+MaoPou@users.noreply.github.com>
2024-09-26 22:01:38 +0800

    bye GameplayGroup.hx

--- source/options/group/GameplayGroup.hx
+++ source/options/group/GameplayGroup.hx
@@ -28,76 +28,9 @@ class GameplayGroup
         follow.addOption(option);
 
         var option:Option = new Option(
-            Language.getStr('flipChart'),
-            'flipChart',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
             Language.getStr('ghostTapping'),
             'ghostTapping',
             BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('guitarHeroSustains'),
-            'guitarHeroSustains',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('noReset'),
-            'noReset',
-            BOOL
-        );
-        follow.addOption(option);
-
-        ///////////////////////////////
-
-        var option:Option = new Option(
-            Language.getStr('Opponent'),
-            TEXT
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('playOpponent'),
-            'playOpponent',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('opponentCodeFix'),
-            'opponentCodeFix',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('botOpponentFix'),
-            'botOpponentFix',
-            BOOL
-        );
-        follow.addOption(option);    
-
-        var option:Option = new Option(
-            Language.getStr('HealthDrainOPPO'),
-            'HealthDrainOPPO',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('HealthDrainOPPOMult'),
-            'HealthDrainOPPOMult',
-            FLOAT,
-            0,
-            5,
-            1
         );
         follow.addOption(option);
     }
