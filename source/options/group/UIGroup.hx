bd3cf78a855550336636958d77186eaf66afa630
MaoPou <166735337+MaoPou@users.noreply.github.com>
2024-09-27 12:27:28 +0800

    Update UIGroup.hx

--- source/options/group/UIGroup.hx
+++ source/options/group/UIGroup.hx
@@ -15,49 +15,6 @@ class UIGroup
         ///////////////////////////////
 
         var option:Option = new Option(
-            Language.getStr('Visble'),
-            TEXT
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('hideHud'),
-            'hideHud',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('showComboNum'),
-            'showComboNum',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('showRating'),
-            'showRating',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('judgementCounter'),
-            'judgementCounter',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('keyboardDisplay'),
-            'keyboardDisplay',
-            BOOL
-        );
-        follow.addOption(option);
-
-        ///////////////////////////////
-
-        var option:Option = new Option(
             Language.getStr('TimeBar'),
             TEXT
         );
@@ -73,25 +30,5 @@ class UIGroup
         follow.addOption(option);
 
         ///////////////////////////////
-
-        var option:Option = new Option(
-            Language.getStr('Camera'),
-            TEXT
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('camZooms'),
-            'camZooms',
-            BOOL
-        );
-        follow.addOption(option);
-
-        var option:Option = new Option(
-            Language.getStr('scoreZoom'),
-            'scoreZoom',
-            BOOL
-        );
-        follow.addOption(option);
     }
 }
